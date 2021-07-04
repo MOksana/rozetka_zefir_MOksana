ac3e21093cea8f24d847f944845060332e938897
MOksana <86306186+MOksana@users.noreply.github.com>
2021-07-04 15:44:08 +0300

    change1

---
+++ js/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+		var sliderOptions = {
+  
+  	prevArrow: '<div class="prev-button"> <svg> <use href="#shevron-right"> </use> </svg></div>',
+  	nextArrow: '<div class="next-button"> <svg> <use href="#shevron-right"> </use> </svg> </div>'
+  };
+  $('.home__content-slider').slick(sliderOptions)
+});
+
+
+
+
+$(document).on('click', '.header__search form button', function(){
+	var searchTerm = $('.header__search form input').val();
+	alert(searchTerm);
+});
+
+ $(document).on('click', '.header__menu-toggle', function(){
+
+
+$(this).toggleClass('opened');
+
+//Змінюєм клас для меню
+$('.slide-menu').toggleClass('opened');
+})
+
+
+	
+
+
+
