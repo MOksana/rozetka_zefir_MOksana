35dbee85577c27c7026be659b91cb7a682386d9e
MOksana <86306186+MOksana@users.noreply.github.com>
2021-07-07 13:57:57 +0300

    up

--- js/app.js
+++ js/app.js
@@ -1,19 +1,24 @@
 var favouriteProducts = [];
+
 //отримуємо раніше збережені у браузері обрані продукти
 if (window.localStorage.getItem('product')) {
 	favouriteProducts = window.localStorage.getItem('product');
-	console.log(favouriteProducts);
 	favouriteProducts = favouriteProducts.split(',');
 }
 updateFavouritesCounter();
+checkIfFavourites();
+function checkIfFavourites() {
+	//Запускаємо функцію для кожної кнопки при завантаженні сторінки
+	$('button.add-to-favourites').each(function() {
+		var product_id = $(this).attr('data-id');
 
-$('button.add-to-favourites').each(function() {
-	var product_id = $(this).attr('data-id');
-
-	if (favouriteProducts.includes(product_id) == true) {
-		$(this).addClass('in-favourites');
-	}
+// перевіряємо чи айді товару є у списку обраних
+if (favouriteProducts.includes(product_id) == true) {
+	$(this).addClass('in-favourites');
+}
 });
+}
+
 
 $(document).on ('click','button.add-to-favourites',function(){
 	//отримаємо ID  продукту, який додаємо в обрані
@@ -24,69 +29,62 @@ $(document).on ('click','button.add-to-favourites',function(){
 	//var currentCount = Number($('#favourites span').html()); 
 
 	if (favouriteProducts.includes(product_id) == true){
-
 	// прибираємо зі списку обраних 
-	// віднімаємо  одиницю від лічильника обраних
+	// визначаємо позицію елемента у масиві
 	var index = favouriteProducts.indexOf(product_id);
 
-//приховуємо злите серце
+//приховуємо залите серце
 $(this).removeClass('in-favourites');
-favouriteProducts.splice(index,1);
-	//currentCount--;
+
+// Видаляємо айді продукту з масив за індексом
+favouriteProducts.splice(index, 1);
+// Віднімаємо одиницю від лічильника обраних
+  	//currentCount--;
 
 } else {
+	//Додаємо айді товару в масив (список) обраних
 	favouriteProducts.push(product_id);
+
+	// Показуємо залите серце
 	$(this).addClass('in-favourites');
 
+//додаємо одиницю до лічильника обраних
+//currentCount = currentCount + 1;
+//currentCount++;
+
+// Зберігаємо список обраних у браузері
 	window.localStorage.setItem('product', favouriteProducts);
 	
-updateFavouritesCounter();
+	updateFavouritesCounter();
 
+
+    //Pure JS
+	// document.getElementById('favourites').innerHTML = '<span>'+currentCount+'</span>';
+	
 	console.log(favouriteProducts);
-}
+});
 
 function updateFavouritesCounter(){
-$('#favourites span').html(favouriteProducts.length);
-if (favouriteProducts.lengt == 0){
-	$('#favourite span').addClass('empty');
-} else {
-	$('#favourite span').removeClass('empty');
-}
+	//ОНОВИТИ ДАНІ В HTML ЛІЧИЛЬНИКА
+	$('#favourites span').html(favouriteProducts.length);
+	if (favouriteProducts.lengt == 0){
+		//сховати лічильник
+		$('#favourites span').addClass('empty');
+	} else {
+		$('#favourites span').removeClass('empty');
+	}
 
 }
 
-//додаємо  ID продукти в масив список обраних
-
-console.log(favouriteProducts);
 
 
-
-
-//оновити дані в HTML лічильника
-//jQuery
-//
-// JS 
-//document
-//if (currentCount==0) {
-	
-	
-
-
-// Показати лічильник
-//$('#favourites span').removeClass('empty');
-
-//alert (currentCount); забираємо спливаюче вікно
-
-
-
-
-
-
+// Home banner slider
 $(document).ready(function(){
 	var sliderOptions = {
 		prevArrow:'<div class="prev-button"><svg><use href="#right-chevron"></use></svg></div>',
-		nextArrow:'<div class="next-button"><svg><use href="#right-chevron"></use></svg></div>',
+		nextArrow:'<div class="next-button"><svg><use href="#right-chevron"></use></svg></div>'
 	};
+
 	$('.home__banner-slider').slick(sliderOptions);
 });
 
@@ -105,7 +103,7 @@ $(document).ready(function(){
 				alert(searchTerm);
 			});
 
-			$(document).on('click','.header__menu-toggle',function(){
+			$(document).on('click','.header__menu-toggle', function(){
              //$(this).addClass('opened');
              //if($(this).hasClass('opened')){
              	//$(this).removeClass('opened'); 
@@ -118,56 +116,58 @@ $(document).ready(function(){
 
             // змінюємо клас для меню
             $('.slide-menu').toggleClass('opened');
-        })
+        });
 
 
 			// var firstName = "Оксана";
 			// var lastName = "Мушинська";
 			// var fullName = firstName+' '+lastName;
 
-			//var myData ={
-				//firstName:'Оксана',
-				//lastName:'Мушинська',
-				//age:35
-			//}
+			var myData ={
+				firstName:'Оксана',
+				lastName:'Мушинська',
+				age:35
+			}
 
-			//function fullNameFunction(first,last){
-				//var fullName = first+' '+last;
-				//return fullName;
-			//}
+			function fullNameFunction(first,last){
+				var fullName = first+' '+last;
+				return fullName;
+			}
 			
-			//var myFullName = fullNameFunction(myData.firstName, myData.lastName);
+			var myFullName = fullNameFunction(myData.firstName, myData.lastName);
 
             //alert(myFullName);
+
+
             function createProductHtml(product) {
             	var html = `<li class="product-card home__product-list-card">
-									<div class="thumb">
-										<button data-id="${product.id}"class="add-to-favourites">
-											<svg>
-											<use href="#heart"></use>
-										</svg> <svg class='filled'>
-											<use href="#heart1"></use>
-										</svg> 
-										</button>
-										
-										<a href="#">
-											<img src="${product.img}">
-										</a>
-										
-									</div>
-									<a href="#" class="title">
-										${product.title}
-									</a>
-									<div class="old price">
-										${product.price * 1.2} 
-									</div>
-									<div class="price">
-										${product.price}
-									</div>
-									<div class="availablity">
-										немає в наявності
-									</div>
-								</li>>`;
+            	<div class="thumb">
+            	<button data-id="${product.id}"class="add-to-favourites">
+            	<svg>
+            	<use href="#heart"></use>
+            	</svg> <svg class='filled'>
+            	<use href="#heart1"></use>
+            	</svg> 
+            	</button>
+
+            	<a href="#">
+            	<img src="${product.img}">
+            	</a>
+
+            	</div>
+            	<a href="#" class="title">
+            	${product.title}
+            	</a>
+            	<div class="old price">
+            	${product.price * 1.2} 
+            	</div>
+            	<div class="price">
+            	${product.price}
+            	</div>
+            	<div class="availablity">
+            	немає в наявності
+            	</div>
+            	</li>>`;
             	return html;
             }
 
@@ -180,20 +180,21 @@ $(document).ready(function(){
             	.then((response)=>{
             		return response.json();
             	})
-            	.then((products)=> {
+            	.then((products)=>{
             		var allHtml = '';
             		var count = 0;
             		products.forEach((product)=>{
             			if(count < 4){
-            			var html = createProductHtml(product);
-            			allHtml = allHtml + html;
+            				var html = createProductHtml(product);
+            				allHtml = allHtml + html;
             			}
             		});
             		$('.home__product-list ul').html(allHtml);
+            		checkIfFavourites();
             	})
-                .catch((error)=> {
-                	console.log(error);
-                })
+            	.catch((error)=> {
+            		console.log(error);
+            	})
 
             }
-            getProducts()
+            getProducts();
